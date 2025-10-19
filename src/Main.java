1a69dd3122e52299e3a7c561f0020907c1cec7ef
razvanpotoceanu <razvanpotoceanu4@gmail.com>
2025-10-19 17:48:02 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+//pb1
+        int[] noten = {29, 37, 38, 41, 84, 67};
+
+        int[] notenNichtAusreichend = NotenNichtAusreichender.findeNichtAusreichende(noten);
+//test subpunct1
+        System.out.println("Noten nicht Ausreichend: ");
+        for (int n: notenNichtAusreichend) {
+            System.out.print(n + " ");
+        }
+//test subpunct2
+        System.out.printf("\nDurchschnittswert: %.2f ", NotenDurchschnitt.findeNotenDurchschnitt(noten));
+
+
+        //test subpunct 3
+        int[] abgerundet = NotenAbgerundete.notenAbgerundete(noten);
+        System.out.println("\nAbgerundeten Noten: ");
+        for (int n: abgerundet) {
+            System.out.print(n + " ");
+        }
+
+        //test subpunct 4
+        System.out.println("\nMaximale abgerundete Note: " + MaximaleAbgerundeteNote.findeMaximaleAbgerundeteNote(abgerundet));
     }
 }
