da796805d839b78beecb162a9eecdae8f8fb36c8
razvanpotoceanu <razvanpotoceanu4@gmail.com>
2025-10-19 17:47:09 +0300

    NotenNichtAusreichender

---
+++ src/NotenNichtAusreichender.java
@@ -0,0 +1,16 @@
+public class NotenNichtAusreichender {
+    public static int[] findeNichtAusreichende(int[] noten) {
+        int count = 0;
+        for (int i = 0; i < noten.length; i++)
+        {
+            if (noten[i] < 40) count++;
+        }
+        int[] ausreichend = new int[count];
+        count = 0;
+        for (int i = 0; i < noten.length; i++)
+        {
+            if (noten[i] < 40) ausreichend[count++] = noten[i];
+        }
+        return ausreichend;
+    }
+}
